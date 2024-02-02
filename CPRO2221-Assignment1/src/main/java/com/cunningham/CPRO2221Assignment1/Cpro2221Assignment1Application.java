265486d9fbcc81bde5cd1c837ecffca3ece10d78
NegrebLC <BergCu2001@gmail.com>
2024-02-01 17:34:05 -0700

    Initial

---
+++ CPRO2221-Assignment1/src/main/java/com/cunningham/CPRO2221Assignment1/Cpro2221Assignment1Application.java
@@ -0,0 +1,13 @@
+package com.cunningham.CPRO2221Assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cpro2221Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cpro2221Assignment1Application.class, args);
+	}
+
+}
---
+++ CPRO2221-Assignment1/src/test/java/com/cunningham/CPRO2221Assignment1/Cpro2221Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cunningham.CPRO2221Assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cpro2221Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
