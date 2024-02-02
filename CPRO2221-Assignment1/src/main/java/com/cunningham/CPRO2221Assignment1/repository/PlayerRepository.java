5bd47c483b3ca0811c71a8e59c596d1faab60ccb
NegrebLC <BergCu2001@gmail.com>
2024-02-01 18:02:34 -0700

    Player Repository

--- CPRO2221-Assignment1/src/main/java/com/cunningham/CPRO2221Assignment1/repository/PlayerRepository.java
+++ CPRO2221-Assignment1/src/main/java/com/cunningham/CPRO2221Assignment1/repository/PlayerRepository.java
@@ -1,4 +1,35 @@
 package com.cunningham.CPRO2221Assignment1.repository;
 
+import com.cunningham.CPRO2221Assignment1.entity.Player;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class PlayerRepository {
+    private final List<Player> playerList = new ArrayList<Player>();
+
+    public List<Player> getPlayerList(){
+        return playerList;
+    }
+
+    public Player searchId(int id) {
+        for(Player player : playerList) {
+            if(player.getId() == id) {
+                return playerList.get(id);
+            }
+        }
+        return null;
+    }
+
+    public Player savePlayer(Player player) {
+        Player p = new Player();
+        p.setId(player.getId());
+        p.setName(player.getName());
+        p.setTeamName(player.getTeamName());
+        p.setJerseyNumber(player.getJerseyNumber());
+        playerList.add(p);
+        return p;
+    }
 }
